1874226d2b8705f531a7897aff2eaf16a0a37208
Alex Albright <alexanderjalbright@gmail.com>
2019-01-15 14:17:27 -0500

    read, variable, switch case practice

--- HelloWorldSpring19/Program.cs
+++ HelloWorldSpring19/Program.cs
@@ -7,17 +7,53 @@ namespace HelloWorldSpring19
         static void Main(string[] args)
         {
             
-            Console.WriteLine("Follow the White Rabbit...");
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("");
             MessinRound();
             
         }
         static void MessinRound()
         {
-            int intNum = 5;
-            int intFirstNum = 6;
+            //Variable Declaration
+            double dblTipPercent;
+            double dblTotal;
+            double dblTip;
 
-            Console.WriteLine(intFirstNum);
+            Console.WriteLine("");
+            Console.WriteLine("******** Tip Calculator ********");
+            Console.WriteLine("");
 
+            //Determine bill total
+            Console.WriteLine("How much was the total bill?");
+            dblTotal = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("How much would you like to tip? 15, 18, or 20 percent?");
+            dblTipPercent = Convert.ToDouble(Console.ReadLine());
+            dblTipPercent = dblTipPercent / 100;
+
+            switch (dblTipPercent)
+            {
+                case 0.15:
+                    dblTip = 15;
+                    break;
+                case 0.18:
+                    dblTip = 18;
+                    break;
+                case 0.20:
+                    dblTip = 20;
+                    break;
+                default:
+                    dblTip = 15;
+                    break;
+            }
+
+            //Do maths
+            dblTipPercent = dblTipPercent / 100;
+            dblTip = dblTotal * dblTipPercent;
+
+            //print tip amount
+            Console.WriteLine("Tip: " + dblTip);
+            
 
         }
     }
