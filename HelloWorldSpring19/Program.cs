29799de768e49045b08da3c1b3b7212fcca236e0
Alex Albright <alexanderjalbright@gmail.com>
2019-01-15 11:25:08 -0500

    First commit

---
+++ HelloWorldSpring19/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace HelloWorldSpring19
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("Follow the White Rabbit...");
+            MessinRound();
+            
+        }
+        static void MessinRound()
+        {
+            int intNum = 5;
+            int intFirstNum = 6;
+
+            Console.WriteLine(intFirstNum);
+
+
+        }
+    }
+}
